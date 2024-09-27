2346f6f94f2e60b8eaa8b7c3284ea7635e97264b
semion-jk <jks8210@students.uc-bcf.edu.ph>
2024-09-27 23:27:20 +0800

    sa wakassss

---
+++ app/src/main/java/com/example/cc17_3f_aboutme/aboutJhandelActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cc17_3f_aboutme
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AboutJhandelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_about_jhandel)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.jhandel)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
